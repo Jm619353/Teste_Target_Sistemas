33ba789d7f9f033716797b0aa6929226ec570c05
João Miguel <125094984+Jm619353@users.noreply.github.com>
2024-09-04 14:53:52 -0300

    Update index.js

--- Teste_5/index.js
+++ Teste_5/index.js
@@ -1,11 +1,20 @@
-
-const original = 'exemplo';
+// Palavra pré-determinada
+const original = 'paralelepipedo';
 const invertida = inverterPalavra(original);
 
-// Função para inverter uma palavra
 function inverterPalavra(original) {
-    return original.split('').reverse().join('');
+  // Inicializa a string invertida como vazia
+  let palavraInvertida = ''; 
+  
+  // Percorre a palavra de trás para frente
+  for (let i = original.length - 1; i >= 0; i--) {
+    // Adiciona cada caractere ao final da nova string
+    palavraInvertida += original[i]; 
   }
+  
+  return palavraInvertida;
+}
+
   console.log(`Palavra original: ${original}`); 
   console.log(`Palavra invertida: ${invertida}`); 
   
